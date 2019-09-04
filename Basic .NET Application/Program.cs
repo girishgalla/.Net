755c17f08ee652e0961ab28af0e07961ccc93f8c
Girish <44398188+girishgalla@users.noreply.github.com>
2019-09-04 11:58:08 +0530

    Add files via upload

---
+++ Basic .NET Application/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Basic_.NET_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
